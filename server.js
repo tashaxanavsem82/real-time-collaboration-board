c8f439e6ffea75ba0365c8625f145135f232cfe8
tashaxanavsem82 <swgwull@hotmail.com>
2025-10-04 12:12:34 -0500

    Refactor MongoDB connection to use new connection options.

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/collabora
 
 const connectToDatabase = async () => {
   try {
-    await mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
+    await mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
     console.log('Successfully connected to MongoDB');
   } catch (err) {
     console.error('Error connecting to MongoDB:', err);
